276a77982a6bfd6877e224e6cc57db25106ebcba
gcubsfan08 <93947261+gcubsfan08@users.noreply.github.com>
2021-11-09 21:53:31 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+/*
+ * //TODO: Gabriel Sotomayor
+ * Recursion Assignment
+ * //TODO: 10/4/21
+ * Based on Dogon Pot pseudo code from the book
+ * African Fractals: Modern Computing and Indigenous Design by Ron Eglash
+ */
+
+using System;
+
+namespace DogonPotVariation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Example steps to draw a Dogon-like pot.";
+            new Pot();
+            Console.ReadKey();
+
+        }
+    }
+}
